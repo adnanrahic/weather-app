30c9d94d76e8be0708f386afba4b44f935c5d72e
adnanrahic <ado.raha198@gmail.com>
2017-01-22 16:40:06 +0100

    fu

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/wat', function (req, res) {
+  res.send("Fuck you!");
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
