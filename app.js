7e2f78b959e5327c00168706496b9cb66eb2d72f
adnanrahic <ado.raha198@gmail.com>
2017-01-22 16:31:26 +0100

    favicon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
